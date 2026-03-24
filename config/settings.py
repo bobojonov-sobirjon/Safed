16238cdfc5242380c974f854f9ae033a96e9dc5e
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2026-03-24 23:00:16 +0500

    chnaged

--- config/settings.py
+++ config/settings.py
@@ -195,7 +195,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "/var/www/media/")
 
 # WhiteNoise for static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
